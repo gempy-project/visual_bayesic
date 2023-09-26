d107e874d8c4a720bcde6a48e7176632318040f3
Miguel <miguel@terranigma-solutions.com>
2023-09-26 12:35:17 +0200

    - [TEST] First test for tensorflow probability

--- tests/test_tf_probability.py
+++ tests/test_tf_probability.py
@@ -1,4 +1,11 @@
 ﻿import tensorflow_probability as tfp
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 def test_sample_from_normal():
-    normal = tfd.Normal(loc=0., scale=1.)
+    normal = tfp.distributions.Normal(loc=0., scale=1.)
+    samples = normal.sample(1000)
+    sns.distplot(samples)
+    plt.title("Samples from a standard Normal")
+    plt.show()
