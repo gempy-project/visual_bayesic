272abae2cbdc624c33d084c29a4f3b780d944d2a
Miguel <miguel@terranigma-solutions.com>
2023-09-26 13:12:07 +0200

    - [ENH] Adding first probabilistic framework

--- examples/examples/sampling_normal/sampling_normals.py
+++ examples/examples/sampling_normal/sampling_normals.py
@@ -2,22 +2,18 @@ from argparse import ArgumentParser
 from xai_components.base import SubGraphExecutor
 from xai_components.xai_plotting.dist_plot import VisualizeNormalDistribution
 from xai_components.xai_tensorflow_probability.normal_distribution import NormalDistribution
-from xai_components.xai_utils.utils import Print
 
 def main(args):
     ctx = {}
     ctx['args'] = args
     c_0 = VisualizeNormalDistribution()
     c_1 = NormalDistribution()
-    c_2 = Print()
     c_0.samples = c_1.samples
-    c_0.title.value = 'This is a plot'
-    c_1.loc.value = 0.02
+    c_1.loc.value = 0.1
     c_1.scale.value = 1
     c_1.population.value = 100
-    c_0.next = c_2
-    c_1.next = None
-    c_2.next = None
+    c_0.next = None
+    c_1.next = c_0
     next_component = c_1
     while next_component:
         next_component = next_component.do(ctx)
