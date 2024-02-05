aaa0ab08ef02a78f2c956a1ab11c4c9da16a73d6
Miguel <miguel@terranigma-solutions.com>
2024-02-05 11:06:59 +0100

    Trying to rollback changes on model node

--- xai_components/xai_probabilistic_models/probabilistic_models_I.py
+++ xai_components/xai_probabilistic_models/probabilistic_models_I.py
@@ -4,7 +4,12 @@ from xai_components.base import InArg, OutArg, Component, xai_component, dynalis
 @xai_component(color="#70856f")
 class PyroModel(Component):
     # TODO: Here we need to have multiple models
-    arg1: InArg[dynalist]
+    arg1: InArg[callable]
+    # arg2: InArg[callable]
+    # arg3: InArg[callable]
+    # arg4: InArg[callable]
+    # arg5: InArg[callable]
+
     model: OutArg[callable]
 
     def __init__(self):
@@ -14,7 +19,16 @@ class PyroModel(Component):
 
     def execute(self, ctx) -> None:
         def pyro_model(_):
-            for arg in self.arg1.value():
-                arg()
+            # if self.arg1.value:
+            self.arg1.value()
+
+        # if self.arg2.value:
+        #     self.arg2.value()
+        # if self.arg3.value:
+        #     self.arg3.value()
+        # if self.arg4.value:
+        #     self.arg4.value()
+        # if self.arg5.value:
+        #     self.arg5.value()
 
         self.model.value = pyro_model
